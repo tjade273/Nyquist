df825b1c2a90f0fd40ef7fa73ba89cc3381fe8e0
Tjaden Hess <tjade273@gmail.com>
2024-02-07 13:08:15 -0600

    Initial Commit

---
+++ Signal Backup/Signal_BackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Signal_BackupApp.swift
+//  Signal Backup
+//
+//  Created by Tjaden Hess on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Signal_BackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
